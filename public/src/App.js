d12a72611e13092e6829cdb82af06f171793c82b
Lê Hải Nam <nam.lh@teko.vn>
2020-02-13 10:43:40 +0700

    Update new example

--- public/src/App.js
+++ public/src/App.js
@@ -1,59 +1,88 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { TekoID } from './index'
+import TekoID from 'teko-oauth2'
 
 const App = props => {
-  const [update, forceUpdate] = useState(false)
-  if (TekoID.user.isLoggedIn())
-    return <HomePageView userInfo={TekoID.user.getUserInfo()} />
-  return <LoginView update={update} forceUpdate={forceUpdate} />
-}
+  const [isLoading, setIsLoading] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-const HomePageView = props => {
+  useEffect(() => {
+    TekoID.init({
+      clientId: 'f82a2730cafa497c848253bb211d7910',
+      redirectUri: 'http://localhost:3000',
+      scopes: ['openid', 'profile', 'read:permissions'],
+      oauthDomain: 'https://oauth.develop.tekoapis.net'
+    }).then(() => {
+      setIsLoading(false)
+      setIsLoggedIn(TekoID.user.isLoggedIn())
+    })
+  }, [])
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <a
-          href='http://localhost:3000'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img src={logo} className='App-logo' alt='logo' />
-        </a>
-        <p>User logged in!</p>
-        <p>Current user info: {props.userInfo.sub}</p>
-        <ul>
-          {Object.keys(props.userInfo).map((item, idx) => (
-            <div style={{ fontSize: 15 }} key={idx}>
-              {item}: {props.userInfo[item]}
-            </div>
-          ))}
-        </ul>
-        <p
-          className='App-link'
-          onClick={() => {
-            TekoID.user.logout()
-          }}
-        >
-          Logout
-        </p>
-      </header>
-    </div>
+    <Layout>
+      {isLoading ? (
+        <Loading />
+      ) : isLoggedIn ? (
+        <HomePageView userInfo={TekoID.user.getUserInfo()} />
+      ) : (
+        <LoginView />
+      )}
+    </Layout>
   )
 }
 
-export const LoginView = props => {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p className='App-link' onClick={() => TekoID.user.login()}>
-          Login with Teko
-        </p>
-      </header>
-    </div>
-  )
-}
+const Loading = () => 'Loading user info....'
+
+const HomePageView = props => (
+  <Fragment>
+    <p>User logged in!</p>
+    <p>Current user id: {props.userInfo.sub}</p>
+    <ul>
+      {Object.keys(props.userInfo)
+        .filter(
+          item =>
+            ![
+              'auth_time',
+              'aud',
+              'at_hash',
+              'iss',
+              'meta_data',
+              'nonce',
+              'sid',
+              'sub'
+            ].includes(item)
+        )
+        .map((item, idx) => (
+          <div style={{ fontSize: 15 }} key={idx}>
+            {item}: {props.userInfo[item]}
+          </div>
+        ))}
+    </ul>
+    <p
+      className='App-link'
+      onClick={() => {
+        TekoID.user.logout()
+      }}
+    >
+      Logout
+    </p>
+  </Fragment>
+)
+
+export const LoginView = props => (
+  <p className='App-link' onClick={() => TekoID.user.login()}>
+    Login with Teko
+  </p>
+)
+
+const Layout = props => (
+  <div className='App'>
+    <header className='App-header'>
+      <img src={logo} className='App-logo' alt='logo' />
+      {props.children}
+    </header>
+  </div>
+)
 
 export default App
--- public/src/index.js
+++ public/src/index.js
@@ -4,18 +4,9 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-const { TekoID } = window
-
-TekoID.init({
-  clientId: 'b67d0f95d70545b3a55a492e21f0a276',
-  scopes: ['openid', 'profile']
-}).then(() => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-})
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
-
-export { TekoID }
